86527798288c8b41284b5b37768e6671e1ef1dfc
Phung Dai <daipv@rever.vn>
2022-02-14 21:29:12 +0700

    crud combo

--- src/controllers/comboController.js
+++ src/controllers/comboController.js
@@ -15,6 +15,7 @@ import {
 import {
     paging
 } from '../utils/utils'
+import {isEqual} from 'lodash'
 
 const res = new Response();
 
@@ -106,6 +107,152 @@ export default class ComboController {
             return res.send(ctx);
         } catch (e) {
             Logger.error('getComboList ' + e.message + ' ' + e.stack +' '+ (e.errors && e.errors[0] ? e.errors[0].message : ''));
+            res.setError(`Error`, Constant.instance.HTTP_CODE.InternalError, null, Constant.instance.ERROR_CODE.SERVER_ERROR);
+            return res.send(ctx);
+        }
+    }
+
+    static postCreateCombo = async (ctx, next) => {
+        try {
+            const {
+                link,
+                mediafiles,
+                meta,
+                name,
+                productCode,
+                SEOInfo,
+                status,
+                bannerInfo
+            } = ctx.request.body;
+            let combo = await Combo.create({
+                link,
+                mediafiles,
+                meta,
+                name,
+                productCode,
+                SEOInfo,
+                status,
+                bannerInfo
+            })
+            res.setSuccess(combo, Constant.instance.HTTP_CODE.Created);
+            return res.send(ctx);
+        } catch (e) {
+            Logger.error('postCreateCombo ' + e.message + ' ' + e.stack +' '+ (e.errors && e.errors[0] ? e.errors[0].message : ''));
+            res.setError(`Error`, Constant.instance.HTTP_CODE.InternalError, null, Constant.instance.ERROR_CODE.SERVER_ERROR);
+            return res.send(ctx);
+        }
+    }
+
+    static putUpdateCombo = async (ctx, next) => {
+        try {
+            const { id } = ctx.request.params
+            const {
+                link,
+                mediafiles,
+                meta,
+                name,
+                productCode,
+                SEOInfo,
+                status,
+                bannerInfo
+            } = ctx.request.body;
+            let updateInfo = {};
+            let respCombo = await Combo.findOne({
+                where: {
+                    id: id
+                }
+            })
+            if (!respCombo) {
+                res.setError("Not found", Constant.instance.HTTP_CODE.NotFound);
+                return res.send(ctx);
+            }
+            if (link && link != respCombo.link) {
+                updateInfo.link = link
+            }
+            if (name && name != respCombo.name) {
+                updateInfo.name = name
+            }
+            if (status && status != respCombo.status) {
+                updateInfo.status = status
+            }
+            if (mediafiles && !isEqual(mediafiles, respCombo.mediafiles)) {
+                updateInfo.mediafiles = mediafiles
+            }
+            if (meta && !isEqual(meta, respCombo.meta)) {
+                updateInfo.meta = meta
+            }
+            if (productCode && !isEqual(productCode, respCombo.productCode)) {
+                updateInfo.productCode = productCode
+            }
+            if (SEOInfo && !isEqual(SEOInfo, respCombo.SEOInfo)) {
+                updateInfo.SEOInfo = SEOInfo
+            }
+            if (bannerInfo && !isEqual(bannerInfo, respCombo.bannerInfo)) {
+                updateInfo.bannerInfo = bannerInfo
+            }
+            respCombo = await respCombo.update(updateInfo);
+            // Return info
+            res.setSuccess(respCombo, Constant.instance.HTTP_CODE.Success);
+            return res.send(ctx);
+            res.setSuccess(combo, Constant.instance.HTTP_CODE.Created);
+            return res.send(ctx);
+        } catch (e) {
+            Logger.error('putUpdateCombo ' + e.message + ' ' + e.stack +' '+ (e.errors && e.errors[0] ? e.errors[0].message : ''));
+            res.setError(`Error`, Constant.instance.HTTP_CODE.InternalError, null, Constant.instance.ERROR_CODE.SERVER_ERROR);
+            return res.send(ctx);
+        }
+    }
+
+    static deleteCombo = async (ctx, next) => {
+        try {
+            const { id } = ctx.request.params
+            let respCombo = await Combo.findOne({
+                where: {
+                    id: id
+                }
+            })
+            if (!respCombo) {
+                res.setError("Not found", Constant.instance.HTTP_CODE.NotFound);
+                return res.send(ctx);
+            }
+            await Combo.destroy({
+                where: {
+                    id:id
+                }
+            })
+            res.setSuccess({deleted: true}, Constant.instance.HTTP_CODE.Success);
+            return res.send(ctx);
+        } catch (e) {
+            Logger.error('deleteCombo ' + e.message + ' ' + e.stack +' '+ (e.errors && e.errors[0] ? e.errors[0].message : ''));
+            res.setError(`Error`, Constant.instance.HTTP_CODE.InternalError, null, Constant.instance.ERROR_CODE.SERVER_ERROR);
+            return res.send(ctx);
+        }
+    }
+
+    static deleteJewelleryInCombo = async (ctx, next) => {
+        try {
+            const { id, jewelleryId } = ctx.request.params
+            let respCombo = await Combo.findOne({
+                where: {
+                    id: id
+                }
+            })
+            if (!respCombo) {
+                res.setError("Not found", Constant.instance.HTTP_CODE.NotFound);
+                return res.send(ctx);
+            }
+            let updateInfo = {}
+            let index = respCombo.productCode.findIndex(item => item === jewelleryId)
+            if(index !== -1) {
+                respCombo.productCode.splice(index, 1)
+                updateInfo.productCode = respCombo.productCode
+            }
+            respCombo = await respCombo.update(updateInfo);
+            // Return info
+            res.setSuccess(respCombo, Constant.instance.HTTP_CODE.Success);
+            return res.send(ctx);
+        } catch (e) {
+            Logger.error('deleteJewelleryInCombo ' + e.message + ' ' + e.stack +' '+ (e.errors && e.errors[0] ? e.errors[0].message : ''));
             res.setError(`Error`, Constant.instance.HTTP_CODE.InternalError, null, Constant.instance.ERROR_CODE.SERVER_ERROR);
             return res.send(ctx);
         }
