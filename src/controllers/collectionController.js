de457bd3bbf78136c4d8e77cdbae49fe8be741c3
Quan Tran <quantran@lecle.co.kr>
2022-03-17 23:00:28 +0700

    fix

--- src/controllers/collectionController.js
+++ src/controllers/collectionController.js
@@ -376,6 +376,16 @@ export default class CollectionController {
                 status,
                 bannerInfo
             } = ctx.request.body;
+            let checkCollection = await Collection.findOne({
+                where:{
+                    link : link
+                }
+            });
+            if (checkCollection)
+            {
+                res.setError("Conflict link", Constant.instance.HTTP_CODE.Conflict);
+                return res.send(ctx);
+            }
             let collection = await Collection.create({
                 link,
                 mediafiles,
@@ -387,16 +397,6 @@ export default class CollectionController {
                 bannerInfo,
                 createdBy: ctx.state.user.id,
             })
-            let checkCollection = await Collection.findOne({
-                where:{
-                    link : link
-                }
-            });
-            if (checkCollection)
-            {
-                res.setError("Conflict link", Constant.instance.HTTP_CODE.Conflict);
-                return res.send(ctx);
-            }
             res.setSuccess(collection, Constant.instance.HTTP_CODE.Created);
             return res.send(ctx);
         } catch (e) {
